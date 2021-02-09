d949cffe791f1d50476baad1d49b05f3b8ad6e61
arabelne <nelson.arabel@lamercantil.com.ar>
2021-02-09 20:57:54 -0300

    Base de proyecto

---
+++ src/main/java/com/narabel/keycloack/ApiKeycloackApplication.java
@@ -0,0 +1,13 @@
+package com.narabel.keycloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKeycloackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKeycloackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narabel/keycloack/ApiKeycloackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narabel.keycloack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKeycloackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
